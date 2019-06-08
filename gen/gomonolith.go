9954cd2f87d40220d6b30677b6c7dacc1219e172
goFrendiAsgard <gofrendiasgard@gmail.com>
2019-06-08 16:33:30 +0700

    prepare gomonolith

--- gen/gomonolith.go
+++ gen/gomonolith.go
@@ -1,26 +1,45 @@
 package gen
 
 import (
+	"fmt"
 	"github.com/state-alchemists/ayanami/generator"
+	"log"
+	"path/filepath"
 )
 
-// GoMonolithProcedure procedureuration to generate GoMonolith
-type GoMonolithProcedure struct {
-	PackageName string
-	*generator.IOHelper
+// GoMonolithProc procedureuration to generate GoMonolith
+type GoMonolithProc struct {
+	ServiceName string
+	RepoName    string
+	generator.IOHelper
 }
 
 // Validate validating procedure
-func (procedure GoMonolithProcedure) Validate(config generator.CommonConfig) bool {
+func (procedure GoMonolithProc) Validate(configs generator.Configs) bool {
 	return true
 }
 
 // Scaffold scaffolding procedure
-func (procedure GoMonolithProcedure) Scaffold(config generator.CommonConfig) error {
+func (procedure GoMonolithProc) Scaffold(configs generator.Configs) error {
 	return nil
 }
 
 // Build building procedure
-func (procedure GoMonolithProcedure) Build(config generator.CommonConfig) error {
-	return nil
+func (procedure GoMonolithProc) Build(configs generator.Configs) error {
+	log.Printf("[INFO] Building %s", procedure.ServiceName)
+	depPath := fmt.Sprintf(procedure.ServiceName)
+	// write go.mod
+	log.Println("[INFO] Create go.mod")
+	goModPath := filepath.Join(depPath, "go.mod")
+	err := procedure.WriteDep(goModPath, "go.mod", procedure)
+	return err
+}
+
+// NewGoMonolith make monolithic app
+func NewGoMonolith(g *generator.Generator, serviceName, repoName string) GoMonolithProc {
+	return GoMonolithProc{
+		ServiceName: serviceName,
+		RepoName:    repoName,
+		IOHelper:    g.IOHelper,
+	}
 }
