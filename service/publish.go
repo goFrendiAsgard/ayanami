0309339668925764865f2d4fd38bb217eff059c2
goFrendiAsgard <gofrendiasgard@gmail.com>
2019-06-14 16:37:54 +0700

    publish data instead of pointer, also publish struct fields

--- service/publish.go
+++ service/publish.go
@@ -13,22 +13,42 @@ func Publish(serviceName, methodName string, broker msgbroker.CommonBroker, ID, 
 	return publish(serviceName, methodName, broker, ID, eventName, data, 10)
 }
 
-func publish(serviceName, methodName string, broker msgbroker.CommonBroker, ID, eventName string, data interface{}, level int) error {
-	if level <= 0 {
-		return nil
+func getReflectAndRealData(data interface{}) (reflect.Value, reflect.Kind, interface{}) {
+	reflectVal := reflect.ValueOf(data)
+	reflectKind := reflectVal.Kind()
+	if reflectKind == reflect.Ptr {
+		reflectVal = reflectVal.Elem()
+		reflectKind = reflectVal.Kind()
+		if reflectVal.IsValid() {
+			data = reflectVal.Interface()
+		} else {
+			data = nil
+		}
 	}
-	pkg := servicedata.Package{ID: ID, Data: data}
+	return reflectVal, reflectKind, data
+}
+
+func getServicePlusMethodName(serviceName, methodName string) string {
 	servicePlusMethod := serviceName
 	if methodName != "" {
 		servicePlusMethod = fmt.Sprintf("%s.%s", serviceName, methodName)
 	}
+	return servicePlusMethod
+}
+
+func publish(serviceName, methodName string, broker msgbroker.CommonBroker, ID, eventName string, data interface{}, level int) error {
+	if level <= 0 {
+		return nil
+	}
+	servicePlusMethod := getServicePlusMethodName(serviceName, methodName)
+	reflectVal, reflectKind, data := getReflectAndRealData(data)
+	// prepare package
+	pkg := servicedata.Package{ID: ID, Data: data}
 	log.Printf("[INFO: %s] Publish `%s`: %#v", servicePlusMethod, eventName, pkg)
 	err := broker.Publish(eventName, pkg)
 	if err != nil {
 		return err
 	}
-	reflectVal := reflect.ValueOf(data)
-	reflectKind := reflectVal.Kind()
 	// publish sub data recursively
 	switch reflectKind {
 	case reflect.Map:
@@ -46,6 +66,16 @@ func publish(serviceName, methodName string, broker msgbroker.CommonBroker, ID, 
 		for index := 0; index < reflectVal.Len(); index++ {
 			subData := reflectVal.Index(index).Interface()
 			subEventName := fmt.Sprintf("%s.%d", eventName, index)
+			err := publish(serviceName, methodName, broker, ID, subEventName, subData, level-1)
+			if err != nil {
+				return err
+			}
+		}
+	case reflect.Struct:
+		for index := 0; index < reflectVal.NumField(); index++ {
+			subData := reflectVal.Field(index).Interface()
+			subVarName := reflectVal.Type().Field(index).Name
+			subEventName := fmt.Sprintf("%s.%s", eventName, subVarName)
 			err := publish(serviceName, methodName, broker, ID, subEventName, subData, level-1)
 			if err != nil {
 				return err
